7876a6e73497e1004a2065cac0d2fb0fa8b70da3
danieljanowski <daniel.janowski@gmail.com>
2021-01-15 18:51:59 +0000

    added launch and landing success filtering

--- pages/components/Search.js
+++ pages/components/Search.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 function Search({ handleSearchDataChange }) {
   return (
-    <div>
-      <p>This is search component</p>
+    <div className="">
       {/* <form onSubmit={(e) => e.preventDefault()} className="search"> */}
       {/* <label htmlFor="textSearch">Search</label> */}
       <input
@@ -13,6 +12,27 @@ function Search({ handleSearchDataChange }) {
         placeholder="Search"
         name="textSearch"
       />
+      <select
+        onChange={handleSearchDataChange}
+        className="launch-success-dropdown"
+        placeholder="Launch success"
+        name="launchSuccess"
+      >
+        <option value="no choice">Launch success choice</option>
+        <option value="yes">Launch successful</option>
+        <option value="no">Launch unsuccessful</option>
+      </select>
+
+      <select
+        onChange={handleSearchDataChange}
+        className="landing-success-dropdown"
+        placeholder="Landing success"
+        name="landingSuccess"
+      >
+        <option value="no choice">Landing success choice</option>
+        <option value="yes">Landing successful</option>
+        <option value="no">Landing unsuccessful</option>
+      </select>
       {/* </form> */}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -30,13 +30,47 @@ export default function Home({ pastMissionsData }) {
     setSearchData({ ...searchData, [e.target.name]: e.target.value });
   };
 
-  const searchResults = () =>
-    pastMissionsData.filter(
+  const searchResults = () => {
+    let selectorSearchResult = {};
+    switch (searchData.launchSuccess) {
+      case 'yes':
+        selectorSearchResult = pastMissionsData.filter(
+          (mission) => mission.launch_success === true,
+        );
+        break;
+      case 'no':
+        selectorSearchResult = pastMissionsData.filter(
+          (mission) => mission.launch_success === false,
+        );
+        break;
+      default:
+        selectorSearchResult = pastMissionsData;
+    }
+
+    switch (searchData.landingSuccess) {
+      case 'yes':
+        selectorSearchResult = selectorSearchResult.filter(
+          (mission) =>
+            mission.rocket.first_stage.cores[0].land_success === true,
+        );
+        break;
+      case 'no':
+        selectorSearchResult = selectorSearchResult.filter(
+          (mission) =>
+            mission.rocket.first_stage.cores[0].land_success === false,
+        );
+        break;
+      default:
+        break;
+    }
+
+    return selectorSearchResult.filter(
       (mission) =>
         `${mission.mission_name} ${mission.details}`
           .toLowerCase()
           .search(searchData.textSearch.toLowerCase()) >= 0,
     );
+  };
 
   return (
     <div className={styles.container}>
